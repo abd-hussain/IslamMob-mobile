cc49c098d89714d251d70810bfdd662fc2e6531c
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-09-07 21:51:36 +0300

    fix android

--- android/app/src/main/kotlin/com/example/islam_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/islam_app/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.islam_app
+package com.islamapp.app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
