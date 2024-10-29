8caa74bb85b119acf5c2ad1bcb37d7b633061600
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-10-29 12:37:12 +0300

    handle android but you need to wait 24 Hour

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:islam_app/my_app/my_app.dart';
 import 'package:islam_app/utils/logger.dart';
 
-//TODO: check coloring and logo
-
 void main() {
   logDebugMessage(message: 'Application Started ...');
   runApp(const MyApp());
