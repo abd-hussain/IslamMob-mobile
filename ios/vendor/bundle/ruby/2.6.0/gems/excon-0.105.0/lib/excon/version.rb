792a66dd8af92bb440665cc14b2e4857978def11
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-10-30 12:29:57 +0300

    try iOS

--- ios/vendor/bundle/ruby/2.6.0/gems/excon-0.105.0/lib/excon/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Excon
-  VERSION = '0.105.0'
-end
--- ios/vendor/bundle/ruby/2.6.0/gems/fastlane-plugin-flutter_version-1.1.15/lib/fastlane/plugin/flutter_version/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Fastlane
-  module FlutterVersion
-    VERSION = '1.1.15'
-  end
-end
