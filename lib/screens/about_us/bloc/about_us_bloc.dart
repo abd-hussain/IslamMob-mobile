e5e0c47048700178cd822a70b87abcb3c8908bda
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-11-04 22:01:59 +0300

    finish handle setting tab

---
+++ lib/screens/about_us/bloc/about_us_bloc.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:islam_app/utils/adds_helper.dart';
+
+part 'about_us_event.dart';
+part 'about_us_state.dart';
+part 'about_us_bloc.freezed.dart';
+
+class AboutUsBloc extends Bloc<AboutUsEvent, AboutUsState> {
+  int _numRewardedLoadAttempts = 0;
+  AboutUsBloc() : super(const AboutUsState()) {
+    on<_UpdateRewardedAd>(_updateRewardedAd);
+
+    _createRewardedAd();
+  }
+
+  void _createRewardedAd() {
+    RewardedAd.load(
+      adUnitId: AdHelper.rewardedAdUnitId,
+      request: const AdRequest(),
+      rewardedAdLoadCallback:
+          RewardedAdLoadCallback(onAdLoaded: (RewardedAd rewardedAd) {
+        _numRewardedLoadAttempts = 0;
+        add(AboutUsEvent.updateRewardedAd(rewardedAd));
+      }, onAdFailedToLoad: (LoadAdError error) {
+        add(AboutUsEvent.updateRewardedAd(null));
+        _numRewardedLoadAttempts += 1;
+        if (_numRewardedLoadAttempts < 3) {
+          _createRewardedAd();
+        }
+      }),
+    );
+  }
+
+  void showRewardedAd(RewardedAd rewardedAd) {
+    rewardedAd.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (RewardedAd ad) =>
+          debugPrint('ad onAdShowedFullScreenContent.'),
+      onAdDismissedFullScreenContent: (RewardedAd ad) {
+        debugPrint('$ad onAdDismissedFullScreenContent.');
+        ad.dispose();
+        _createRewardedAd();
+      },
+      onAdFailedToShowFullScreenContent: (RewardedAd ad, AdError error) {
+        debugPrint('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+        _createRewardedAd();
+      },
+    );
+
+    rewardedAd.setImmersiveMode(true);
+    rewardedAd.show(onUserEarnedReward: (AdWithoutView ad, RewardItem reward) {
+      debugPrint(
+          '$ad with reward $RewardItem(${reward.amount}, ${reward.type})');
+    });
+    add(AboutUsEvent.updateRewardedAd(null));
+  }
+
+  FutureOr<void> _updateRewardedAd(
+      _UpdateRewardedAd event, Emitter<AboutUsState> emit) {
+    emit(state.copyWith(rewardedAd: event.value));
+  }
+}
