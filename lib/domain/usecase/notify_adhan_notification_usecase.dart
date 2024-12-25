b4d4e2917332a6bca873afa38238682306ade5bb
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-12-26 00:34:43 +0300

    .

---
+++ lib/domain/usecase/notify_adhan_notification_usecase.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:islam_app/domain/repository/local_notifications.dart';
+import 'package:islam_app/domain/usecase/pray_manager/pray_usecase.dart';
+import 'package:islam_app/models/pray_timing.dart';
+import 'package:islam_mob_adhan/adhan.dart';
+
+class NotifyAdhanNotificationUsecase {
+  PrayUsecase prayUsecase = PrayUsecase();
+
+  Future<void> prepareLocalNotificationForTomorrow(BuildContext context) async {
+    final tommorrow = DateTime.now().add(const Duration(days: 1));
+    prayUsecase = PrayUsecase(
+        specificDate:
+            DateComponents(tommorrow.year, tommorrow.month, tommorrow.day));
+    PrayTimingDateTimeModel prayTimingDateTimeModel =
+        prayUsecase.getAllPrayTimeAsDateTimeForToday();
+
+    final fajirPrayTime = DateTime(
+      tommorrow.year,
+      tommorrow.month,
+      tommorrow.day,
+      prayTimingDateTimeModel.fajir.hour,
+      prayTimingDateTimeModel.fajir.minute,
+    );
+
+    final sunrisePrayTime = DateTime(
+      tommorrow.year,
+      tommorrow.month,
+      tommorrow.day,
+      prayTimingDateTimeModel.sunrise.hour,
+      prayTimingDateTimeModel.sunrise.minute,
+    );
+
+    final dhuhrPrayTime = DateTime(
+      tommorrow.year,
+      tommorrow.month,
+      tommorrow.day,
+      prayTimingDateTimeModel.dhuhr.hour,
+      prayTimingDateTimeModel.dhuhr.minute,
+    );
+
+    final asrPrayTime = DateTime(
+      tommorrow.year,
+      tommorrow.month,
+      tommorrow.day,
+      prayTimingDateTimeModel.asr.hour,
+      prayTimingDateTimeModel.asr.minute,
+    );
+
+    final maghribPrayTime = DateTime(
+      tommorrow.year,
+      tommorrow.month,
+      tommorrow.day,
+      prayTimingDateTimeModel.maghrib.hour,
+      prayTimingDateTimeModel.maghrib.minute,
+    );
+
+    final ishaPrayTime = DateTime(
+      tommorrow.year,
+      tommorrow.month,
+      tommorrow.day,
+      prayTimingDateTimeModel.isha.hour,
+      prayTimingDateTimeModel.isha.minute,
+    );
+
+    await LocalNotificationRepository.scheduleNotification(
+      id: 1,
+      context: context,
+      type: NotificationType.before15Minutes,
+      scheduledTime: fajirPrayTime.subtract(const Duration(minutes: 15)),
+    );
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 2,
+        context: context,
+        type: NotificationType.fajir,
+        scheduledTime: fajirPrayTime,
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 3,
+        context: context,
+        type: NotificationType.before15Minutes,
+        scheduledTime: sunrisePrayTime.subtract(const Duration(minutes: 15)),
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 4,
+        context: context,
+        type: NotificationType.sunrise,
+        scheduledTime: sunrisePrayTime,
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 5,
+        context: context,
+        type: NotificationType.before15Minutes,
+        scheduledTime: dhuhrPrayTime.subtract(const Duration(minutes: 15)),
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 6,
+        context: context,
+        type: NotificationType.zuhr,
+        scheduledTime: dhuhrPrayTime,
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 7,
+        context: context,
+        type: NotificationType.before15Minutes,
+        scheduledTime: asrPrayTime.subtract(const Duration(minutes: 15)),
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 8,
+        context: context,
+        type: NotificationType.asr,
+        scheduledTime: asrPrayTime,
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 9,
+        context: context,
+        type: NotificationType.before15Minutes,
+        scheduledTime: maghribPrayTime.subtract(const Duration(minutes: 15)),
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 10,
+        context: context,
+        type: NotificationType.maghrib,
+        scheduledTime: maghribPrayTime,
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 11,
+        context: context,
+        type: NotificationType.before15Minutes,
+        scheduledTime: ishaPrayTime.subtract(const Duration(minutes: 15)),
+      );
+    }
+    if (context.mounted) {
+      await LocalNotificationRepository.scheduleNotification(
+        id: 12,
+        context: context,
+        type: NotificationType.isha,
+        scheduledTime: ishaPrayTime,
+      );
+    }
+  }
+}
