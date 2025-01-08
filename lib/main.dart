13d0809deaaed1a5d16f5fcbb2b62b35ead43e3b
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2025-01-08 21:24:23 +0300

    cancel all notification and prepare sounds

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:islam_app/my_app/my_app.dart';
 import 'package:logger_manager/logger_manager.dart';
 
-//TODO: local notification sound not working
-//TODO: cancel all of the scheduled notification when app is open
 //TODO: fire a new notification schaduled when app is open
 
 void main() {
   LoggerManagerBase.logDebugMessage(message: 'Application Started ...');
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(const MyApp());
 }
