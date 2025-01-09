bff9e9c81f4f335258748f628b394e8972a21139
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2025-01-09 14:26:16 +0300

    apply asr adhan

--- utilities/database_manager/lib/src/database_manager_base.dart
+++ utilities/database_manager/lib/src/database_manager_base.dart
@@ -10,16 +10,19 @@ class DataBaseManagerBase {
     await Hive.openBox(DatabaseBoxConstant.userInfo);
   }
 
-  static dynamic getFromDatabase({required String key, required dynamic defaultValue}) {
+  static dynamic getFromDatabase(
+      {required String key, required dynamic defaultValue}) {
     return _userBox.get(key, defaultValue: defaultValue);
   }
 
-  static Future<void> saveInDatabase({required String key, required dynamic value}) async {
+  static Future<void> saveInDatabase(
+      {required String key, required dynamic value}) async {
     return await _userBox.put(key, value);
   }
 
   /// Updates multiple values in Hive storage
-  static Future<void> saveMultipleInDatabase({required Map<String, dynamic> data}) async {
+  static Future<void> saveMultipleInDatabase(
+      {required Map<String, dynamic> data}) async {
     for (var entry in data.entries) {
       await _userBox.put(entry.key, entry.value);
     }
