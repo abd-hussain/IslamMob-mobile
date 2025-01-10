a864bdac0ef9d7cab01a7dee25e83419ac104afc
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2025-01-10 14:14:02 +0300

    handle notification for all other types

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:islam_app/my_app/my_app.dart';
 import 'package:logger_manager/logger_manager.dart';
 
+//TODO: handle countdown notification for android
+//TODO: theres a case where the location shown invalid or empty
+
 void main() {
   LoggerManagerBase.logDebugMessage(message: 'Application Started ...');
   WidgetsFlutterBinding.ensureInitialized();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import firebase_messaging
-import firebase_remote_config
 import firebase_storage
 import flutter_local_notifications
 import geolocator_apple
@@ -24,7 +23,7 @@ import rate_my_app
 import screen_brightness_macos
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import webview_flutter_wkwebview
 
@@ -37,7 +36,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
