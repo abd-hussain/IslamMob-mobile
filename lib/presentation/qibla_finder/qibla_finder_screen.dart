a294254959e3f1b4a3f37233981e61030403f785
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2025-03-30 02:13:32 +0300

    try android 5

--- lib/presentation/qibla_finder/qibla_finder_screen.dart
+++ lib/presentation/qibla_finder/qibla_finder_screen.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:islam_app/l10n/gen/app_localizations.dart';
 import 'package:islam_app/presentation/qibla_finder/bloc/qibla_finder_bloc.dart';
-import 'package:islam_app/presentation/qibla_finder/widgets/compass/qiblah_compass.dart';
-import 'package:islam_app/presentation/qibla_finder/widgets/map/qiblah_maps.dart';
 import 'package:islam_app/shared_widgets/appbar/custom_appbar.dart';
-import 'package:islam_app/shared_widgets/custom_text.dart';
-import 'package:qibla_finder/qibla_finder.dart';
+// import 'package:qibla_finder/qibla_finder.dart';
 
 class QiblaFinderScreen extends StatelessWidget {
   const QiblaFinderScreen({super.key});
@@ -14,37 +11,35 @@ class QiblaFinderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) =>
-          QiblaFinderBloc()..add(const QiblaFinderEvent.setup()),
+      create: (context) => QiblaFinderBloc()..add(const QiblaFinderEvent.setup()),
       child: Scaffold(
         backgroundColor: const Color(0xfffff2e9),
         appBar: CustomAppBar(
           title: IslamMobLocalizations.of(context).qiblaFinder,
         ),
-        body: SafeArea(
-          child: FutureBuilder(
-            future: QiblahFinder.androidDeviceSensorSupport(),
-            builder: (_, AsyncSnapshot<bool?> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(color: Color(0xff008480)),
-                );
-              }
-              if (snapshot.hasError) {
-                return Center(
-                  child: CustomText(
-                      title: "Error: ${snapshot.error}", fontSize: 16),
-                );
-              }
+        // body: SafeArea(
+        // child: FutureBuilder(
+        //   future: QiblahFinder.androidDeviceSensorSupport(),
+        //   builder: (_, AsyncSnapshot<bool?> snapshot) {
+        //     if (snapshot.connectionState == ConnectionState.waiting) {
+        //       return const Center(
+        //         child: CircularProgressIndicator(color: Color(0xff008480)),
+        //       );
+        //     }
+        //     if (snapshot.hasError) {
+        //       return Center(
+        //         child: CustomText(title: "Error: ${snapshot.error}", fontSize: 16),
+        //       );
+        //     }
 
-              if (snapshot.data!) {
-                return const QiblahCompass();
-              } else {
-                return const QiblahMaps();
-              }
-            },
-          ),
-        ),
+        //     if (snapshot.data!) {
+        //       return const QiblahCompass();
+        //     } else {
+        //       return const QiblahMaps();
+        //     }
+        //   },
+        // ),
+        // ),
       ),
     );
   }
--- lib/presentation/qibla_finder/widgets/compass/qiblah_compass.dart
+++ lib/presentation/qibla_finder/widgets/compass/qiblah_compass.dart
@@ -1,13 +1,5 @@
-import 'dart:async';
-import 'dart:math' show pi;
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:islam_app/presentation/qibla_finder/widgets/compass/compass_footer_view.dart';
-import 'package:islam_app/presentation/qibla_finder/widgets/location_error_widget.dart';
-import 'package:islam_app/shared_widgets/custom_text.dart';
-import 'package:qibla_finder/qibla_finder.dart';
+// import 'package:qibla_finder/qibla_finder.dart';
 
 class QiblahCompass extends StatefulWidget {
   const QiblahCompass({super.key});
@@ -19,7 +11,7 @@ class QiblahCompass extends StatefulWidget {
 class _QiblahCompassState extends State<QiblahCompass> {
   @override
   void dispose() {
-    QiblahFinder().dispose();
+    // QiblahFinder().dispose();
     super.dispose();
   }
 
@@ -28,175 +20,174 @@ class _QiblahCompassState extends State<QiblahCompass> {
     return Container(
       alignment: Alignment.center,
       padding: const EdgeInsets.all(8),
-      child: FutureBuilder(
-        future: _checkLocationStatus(),
-        builder: (context, AsyncSnapshot<LocationStatus> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(color: Color(0xff008480)),
-            );
-          }
+      // child: FutureBuilder(
+      //   future: _checkLocationStatus(),
+      //   builder: (context, AsyncSnapshot<LocationStatus> snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.waiting) {
+      //       return const Center(
+      //         child: CircularProgressIndicator(color: Color(0xff008480)),
+      //       );
+      //     }
 
-          if (snapshot.data!.enabled == true) {
-            switch (snapshot.data!.status) {
-              case LocationPermission.always:
-              case LocationPermission.whileInUse:
-                return const QiblahCompassWidget();
+      //     if (snapshot.data!.enabled == true) {
+      //       switch (snapshot.data!.status) {
+      //         case LocationPermission.always:
+      //         case LocationPermission.whileInUse:
+      //           return const QiblahCompassWidget();
 
-              case LocationPermission.denied:
-              case LocationPermission.deniedForever:
-                return const LocationErrorWidget();
-              default:
-                return const SizedBox();
-            }
-          } else {
-            return const LocationErrorWidget();
-          }
-        },
-      ),
+      //         case LocationPermission.denied:
+      //         case LocationPermission.deniedForever:
+      //           return const LocationErrorWidget();
+      //         default:
+      //           return const SizedBox();
+      //       }
+      //     } else {
+      //       return const LocationErrorWidget();
+      //     }
+      //   },
+      // ),
     );
   }
 
-  Future<LocationStatus> _checkLocationStatus() async {
-    final locationStatus = await QiblahFinder.checkLocationStatus();
-    if (locationStatus.enabled &&
-        locationStatus.status == LocationPermission.denied) {
-      await QiblahFinder.requestPermissions();
-      return QiblahFinder.checkLocationStatus();
-    } else {
-      return locationStatus;
-    }
-  }
+  // Future<LocationStatus> _checkLocationStatus() async {
+  //   final locationStatus = await QiblahFinder.checkLocationStatus();
+  //   if (locationStatus.enabled && locationStatus.status == LocationPermission.denied) {
+  //     await QiblahFinder.requestPermissions();
+  //     return QiblahFinder.checkLocationStatus();
+  //   } else {
+  //     return locationStatus;
+  //   }
+  // }
 }
 
-class QiblahCompassWidget extends StatelessWidget {
-  const QiblahCompassWidget({super.key});
+// class QiblahCompassWidget extends StatelessWidget {
+//   const QiblahCompassWidget({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: QiblahFinder.qiblahStream,
-      builder: (_, AsyncSnapshot<QiblahDirection> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(color: Color(0xff008480)),
-          );
-        }
-        final qiblahDirection = snapshot.data!;
-        // final radios = snapshot.data!;
-        final diameter = MediaQuery.of(context).size.width - 60;
-        final radios = diameter / 1.5;
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder(
+//       stream: QiblahFinder.qiblahStream,
+//       builder: (_, AsyncSnapshot<QiblahDirection> snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return const Center(
+//             child: CircularProgressIndicator(color: Color(0xff008480)),
+//           );
+//         }
+//         final qiblahDirection = snapshot.data!;
+//         // final radios = snapshot.data!;
+//         final diameter = MediaQuery.of(context).size.width - 60;
+//         final radios = diameter / 1.5;
 
-        return Column(
-          children: [
-            const SizedBox(height: 20),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Transform.rotate(
-                  angle: qiblahDirection.direction * (pi / 180) * -1,
-                  child: SvgPicture.asset(
-                    'assets/images/qibla/compass.svg',
-                    width: diameter,
-                    height: diameter,
-                  ),
-                ),
-                Transform.rotate(
-                  angle: qiblahDirection.qiblah * (pi / 180) * -1,
-                  child: SvgPicture.asset(
-                    'assets/images/qibla/needle.svg',
-                    width: radios,
-                    height: radios,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            CompassFooterView(
-              offset: qiblahDirection.offset,
-            ),
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 1,
-                        offset: const Offset(0, 1),
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.location_on,
-                              color: Color(0xff444444),
-                              size: 12,
-                            ),
-                            CustomText(
-                              title: "${qiblahDirection.offset}°",
-                              fontSize: 12,
-                              fontWeight: FontWeight.w600,
-                              color: const Color(0xff444444),
-                            ),
-                          ],
-                        ),
-                        const CustomText(
-                          title: "qibla direction from north",
-                          fontSize: 8,
-                          color: Color(0xff444444),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 1,
-                        offset: const Offset(0, 1),
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(
-                      children: [
-                        CustomText(
-                          title: "${qiblahDirection.direction}°",
-                          fontSize: 12,
-                          fontWeight: FontWeight.w600,
-                          color: const Color(0xff444444),
-                        ),
-                        const CustomText(
-                          title: "direction",
-                          fontSize: 8,
-                          color: Color(0xff444444),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
+//         return Column(
+//           children: [
+//             const SizedBox(height: 20),
+//             Stack(
+//               alignment: Alignment.center,
+//               children: [
+//                 Transform.rotate(
+//                   angle: qiblahDirection.direction * (pi / 180) * -1,
+//                   child: SvgPicture.asset(
+//                     'assets/images/qibla/compass.svg',
+//                     width: diameter,
+//                     height: diameter,
+//                   ),
+//                 ),
+//                 Transform.rotate(
+//                   angle: qiblahDirection.qiblah * (pi / 180) * -1,
+//                   child: SvgPicture.asset(
+//                     'assets/images/qibla/needle.svg',
+//                     width: radios,
+//                     height: radios,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             const SizedBox(height: 20),
+//             CompassFooterView(
+//               offset: qiblahDirection.offset,
+//             ),
+//             Row(
+//               children: [
+//                 Container(
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     border: Border.all(color: Colors.grey),
+//                     borderRadius: BorderRadius.circular(10),
+//                     boxShadow: [
+//                       BoxShadow(
+//                         color: Colors.grey.withOpacity(0.5),
+//                         spreadRadius: 1,
+//                         blurRadius: 1,
+//                         offset: const Offset(0, 1),
+//                       ),
+//                     ],
+//                   ),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(8),
+//                     child: Column(
+//                       children: [
+//                         Row(
+//                           children: [
+//                             const Icon(
+//                               Icons.location_on,
+//                               color: Color(0xff444444),
+//                               size: 12,
+//                             ),
+//                             CustomText(
+//                               title: "${qiblahDirection.offset}°",
+//                               fontSize: 12,
+//                               fontWeight: FontWeight.w600,
+//                               color: const Color(0xff444444),
+//                             ),
+//                           ],
+//                         ),
+//                         const CustomText(
+//                           title: "qibla direction from north",
+//                           fontSize: 8,
+//                           color: Color(0xff444444),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//                 Container(
+//                   decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     border: Border.all(color: Colors.grey),
+//                     borderRadius: BorderRadius.circular(10),
+//                     boxShadow: [
+//                       BoxShadow(
+//                         color: Colors.grey.withOpacity(0.5),
+//                         spreadRadius: 1,
+//                         blurRadius: 1,
+//                         offset: const Offset(0, 1),
+//                       ),
+//                     ],
+//                   ),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(8),
+//                     child: Column(
+//                       children: [
+//                         CustomText(
+//                           title: "${qiblahDirection.direction}°",
+//                           fontSize: 12,
+//                           fontWeight: FontWeight.w600,
+//                           color: const Color(0xff444444),
+//                         ),
+//                         const CustomText(
+//                           title: "direction",
+//                           fontSize: 8,
+//                           color: Color(0xff444444),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
