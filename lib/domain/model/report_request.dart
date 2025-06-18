7d4c7f2e304dd032cb33b1ac08576737f670b326
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2025-06-18 11:43:30 +0300

    last fix

--- lib/domain/model/report_request.dart
+++ lib/domain/model/report_request.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 import 'package:freezed_annotation/freezed_annotation.dart';
+
 part 'report_request.freezed.dart';
-part 'report_request.g.dart';
 
 @freezed
 class ReportRequest with _$ReportRequest {
@@ -12,17 +12,4 @@ class ReportRequest with _$ReportRequest {
     File? attach2,
     File? attach3,
   }) = _ReportRequest;
-}
-
-@freezed
-class ReportRequestToFirebase with _$ReportRequestToFirebase {
-  factory ReportRequestToFirebase({
-    required String content,
-    String? attach1,
-    String? attach2,
-    String? attach3,
-  }) = _ReportRequestToFirebase;
-
-  factory ReportRequestToFirebase.fromJson(Map<String, dynamic> json) =>
-      _$ReportRequestToFirebaseFromJson(json);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:islam_app/my_app/my_app.dart';
 import 'package:logger_manager/logger_manager.dart';
 
+//TODO: upgrade flutter version
 //TODO: show widget out side the app for next prayer time
 //TODO: Support should send to email instedof firestore
 //TODO: kool ma tftaa7 al app lazm y3maal update ll location
