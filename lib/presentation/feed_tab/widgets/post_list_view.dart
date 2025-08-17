24d8f5d1ba2d0cff9cafedf94cedcd8cf0aa0a7a
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2025-08-17 15:43:07 +0300

    try self hosting

--- lib/presentation/feed_tab/widgets/post_list_view.dart
+++ lib/presentation/feed_tab/widgets/post_list_view.dart
@@ -7,11 +7,7 @@ class PostListView extends StatelessWidget {
   final List<Post> postsList;
   final Future<void> Function() onRefresh;
 
-  const PostListView({
-    super.key,
-    required this.postsList,
-    required this.onRefresh,
-  });
+  const PostListView({super.key, required this.postsList, required this.onRefresh});
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +16,12 @@ class PostListView extends StatelessWidget {
             onRefresh: () async => onRefresh(),
             child: ListView.builder(
               shrinkWrap: true,
-              itemCount: postsList.length,
+              itemCount: postsList.length + 1,
               itemBuilder: (context, index) {
+                if (index == postsList.length) {
+                  // This is the extra space at the end
+                  return const SizedBox(height: 85); // adjust the height you want
+                }
                 return PostView(postDetails: postsList[index]);
               },
             ),
